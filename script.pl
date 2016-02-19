4fcb2681340e7654151eea383c6d81233f6af29f
Matheus Bernardo <matheussbernardo@gmail.com>
2016-02-19 03:51:12 -0500

    Creating the basic command line tool

--- script.pl
+++ script.pl
@@ -1,9 +1,44 @@
+#!/usr/bin/perl
 use warnings;
 use strict;
+
 open FILE, "<webpage.html";
-my $courses = do{local $/; <FILE>};
+my $temp = do{local $/; <FILE>};
 #\s*<\/td><td>([A-Z]{2}.*(?:PM|AM))
 
-while ($courses =~ />([A-Z]+-\w+-\w+)<br>.*<\/span><\/a>\s*<\/td><td>(\w+\s+\w+.*(?:AM|PM)\s*)<\/td><td>[A-Z]/g) {
-  print "<$1><$2>\n"
+sub help() {
+  print("\nUsage: perl -w perlproject.pl \"Term\" \"Room\" [> filename.html]\n
+  List of options:
+    \tRequired:
+        \t\tTerm : which Term (semester) the class is in, e.g. \"16/SP - Spring 2016\"
+        \t\tRoom : which classroom
+    \tOptional:
+        \t\t--help : display this help screen and sample usage
+        \t\t--terms : list all currently available\n\n");
 }
+
+sub getRooms() {
+  while ($temp =~ />([A-Z]+-\w+-\w+)<br>.*<\/span><\/a>\s*<\/td><td>(\w+\s+\w+.*(?:AM|PM)\s*)<\/td><td>[A-Z]/g) {
+    print "<$2>\n"
+  }
+}
+
+sub getTerms() {
+  print "teste terms\n"
+}
+
+
+foreach (@ARGV) {
+      if($_ =~ /--terms/) {
+        getTerms();
+        exit 0;
+      }
+      elsif($_ =~ /--rooms/) {
+        getRooms();
+        exit 0;
+      }
+      else {
+        help();
+        exit 0;
+      }
+};
